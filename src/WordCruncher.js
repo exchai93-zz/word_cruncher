5e4cf67532a9261b8238bb813796e5f134b83bfc
Emily Chai <echai93@gmail.com>
2017-06-12 16:46:43 +0200

    WIP implementing countWordFrequencies method

--- src/WordCruncher.js
+++ src/WordCruncher.js
@@ -4,14 +4,27 @@ function WordCruncher(text) {
   this.text = text
 }
 
-WordCruncher.prototype.splitWords = function () {
-  this.text = this.text.split(" ");
-};
-
-WordCruncher.prototype.removePunctuation = function () {
+WordCruncher.prototype.removeNonAlphanumerics = function () {
   this.text = this.text.replace(/[^\w\s]|_/g, "");
 };
 
-WordCruncher.prototype.removeCapitals= function () {
+WordCruncher.prototype.replaceUpperCase= function () {
   this.text = this.text.toLowerCase();
+};
+
+WordCruncher.prototype.splitText = function () {
+  this.text = this.text.split(" ");
+};
+
+WordCruncher.prototype.countWordFrequencies = function (words) {
+
+  var wordCount = {};
+
+  this.text.forEach(function(n) {
+    if (this.wordCount[this.text[n]] !== undefined) {
+      this.wordCount[this.text[n]] ++;
+    } else {
+      this.wordCount[this.text[n]] == 1;
+    }
+  })
 };
