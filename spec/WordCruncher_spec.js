1ae19783ed2e21990802e8b0fe5d9264963ad806
Emily Chai <echai93@gmail.com>
2017-06-13 10:27:07 +0200

    Refactor phrasing of tests

--- spec/WordCruncher_spec.js
+++ spec/WordCruncher_spec.js
@@ -4,11 +4,11 @@ describe('WordCruncher', function() {
   var wc;
 
   beforeEach(function() {
-    var text = "This is a sample text"
+    var text = "This is a sample text";
     wc = new WordCruncher(text);
   });
 
-  describe('initialize', function() {
+  describe('upon initialization', function() {
     it('takes a parameter in a string format', function() {
       expect(wc.text).toEqual("This is a sample text");
     });
@@ -19,37 +19,38 @@ describe('WordCruncher', function() {
 
     it('has an empty wordCount hash', function() {
       expect(wc.wordCount).toEqual({});
-    })
+    });
   });
 
-  describe('string format', function() {
+  describe('#replaceUpperCase', function() {
+    it('replaces upper case with lower case characters', function() {
+      wc.replaceUpperCase();
+      expect(wc.text).toEqual("this is a sample text");
+    });
+  });
+
+  describe('#removeNonAlphanumerics', function() {
     it('strips everything except alphanumeric characters and whitespace', function() {
       wc.text = "This is an example of a string with punctuation .,:;!()$%";
       wc.removeNonAlphanumerics();
       expect(wc.text).toEqual("This is an example of a string with punctuation ");
     });
+  });
 
-    it('replaces upper case with lower case characters', function() {
-      wc.replaceUpperCase();
-      expect(wc.text).toEqual("this is a sample text");
-    });
-
+  describe('#splitText', function() {
     it('splits text into an array of individual words', function() {
       wc.splitText();
-      expect(wc.text[0]).toEqual("This");
-      expect(wc.text[1]).toEqual("is");
+      expect(wc.wordList).toEqual(["This", "is", "a", "sample", "text"]);
     });
   });
 
-  describe('counting number of words', function() {
-    it('returns values of one', function() {
-      wc.removeNonAlphanumerics();
-      wc.replaceUpperCase();
+  describe('#countWordFrequencies', function() {
+    it('counts the number of frequencies for each word', function() {
+      wc.text = "This is a sample text";
       wc.splitText();
-      wc.countWordFrequencies();
-      expect(wc.wordCount).toEqual({"this": 1, "is": 1,
-                                    "a": 1, "sample": 1,
-                                    "text": 1});
+      expect(wc.countWordFrequencies()).toEqual({this: 1, is: 1,
+                                    a: 1, sample: 1,
+                                    text: 1});
     });
   });
 
